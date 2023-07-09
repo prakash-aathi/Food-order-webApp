7e3f2140acaed9135b5c997b4cbc0bdd110f27f5
Prakash A <prakashaathi321@gmail.com>
2023-07-09 16:23:30 +0530

    removed unused import

--- src/main/java/com/example/springapp/model/MenuItem.java
+++ src/main/java/com/example/springapp/model/MenuItem.java
@@ -1,7 +1,6 @@
 package com.example.springapp.model;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
